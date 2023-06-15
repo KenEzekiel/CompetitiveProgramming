4e9d0b90e72d7f1b2c1884cf5ec93d1b3ffc134b
Kenneth Ezekiel <88850771+KenEzekiel@users.noreply.github.com>
2023-06-15 19:13:30 +0700

    Plotting mentee

---
+++ LeetCode/PlottingMentee.py
@@ -0,0 +1,711 @@
+import numpy as np
+
+ganesha = """Ricky Wijaya
+Dama Dhananjaya Daliman
+Ahmad Habibie Marjan
+Ignatius Jhon Hezkiel Chan
+Kerlyn Deslia Andeskar
+Evelyn Yosiana
+Emery Fathan Zwageri
+Abel Apriliani
+Debrina Veisha Rashika W
+Azim Muzakki Hasnan
+M. Rifki Virziadeili Harisman
+Gabriella Keisha Andini
+Ivan Hendrawan Tan
+Jonathan Wiguna
+Enrique Yanuar
+Shazya Audrea Taufik
+Habib Akhmad Al Farisi
+Kristo Anugrah
+Ananda Farhan Raihandra
+Alvin Fadhilah Akmal
+Rashid May
+Khansa Adilla Reva
+Marvel Pangondian
+Serenada Cinta Sunindyo
+Padmanabha Adyaksa Setyanto
+Muhammad Al Thariq Fairuz
+Albert
+Thalita Zahra Sutejo
+Raizan Iqbal Resi
+Dwi Juli Yantono
+Regina Deva Carissa
+Hartanto Luwis
+Farel Winalda
+Abdullah Mubarak
+Tamara Mayranda Lubis
+Naomi Pricilla Agustine
+Aththariq Lisan Q. D. S.
+Muhammad Yusuf Rafi
+Ahmad Mudabbir Arif
+Akbar Al Fattah
+Filbert
+Rafiki Prawhira Harianto
+Josia Ryan Juliandy Silalahi
+Nicolaas Heru Dreandachrista
+Rayhan Fadhlan Azka
+Denise Felicia Tiowanni
+Ammar Naufal
+Fabian Radenta Bangun
+Christopher Brian
+Keanu Amadius Gonza Wrahatno
+Benardo
+Mohammad Nugraha Eka Prawira
+Angelica Kierra Ninta Gurning
+Yusuf Ardian Sandi
+Wilson Yusda
+Clement Nathanael Lim
+Jihan Aurelia
+Muhammad Kevinza Faiz
+Muhammad Yaafi Wasesa Putra
+Adril Putra Merin
+Rici Trisna Putra
+Thea Josephine Halim
+Francesco Michael Kusuma
+Athhar Mahendra Umar
+M. Athaullah Daffa Kusuma M
+Vanson Kurnialim
+Ibrahim Ihsan Rasyid
+Imanuel Sebastian Girsang
+Axelius Davin
+Erwan Poltak Halomoan
+Edbert Eddyson Gunawan
+Natanael Steven Simangunsong
+Juan Alfred Widjaya
+Viktor Arsindiantoro S
+Moh Fairuz Alauddin Yahya
+Naufal Adnan
+Tazkia Nizami
+Raden Francisco Trianto B.
+Daniel Mulia Putra Manurung
+Firsa Athaya Raissa Alifah
+Hayya Zuhailii Kinasih
+Billy Samuel Setiawan
+Nabila Shikoofa Muida
+David Dewanto
+Favian Izza Diasputra
+Muhammad Daffa Ikram
+Muhammad Faiz Atharrahman
+Suthasoma Mahardhika Munthe
+Ahmad Naufal Ramadan
+Yoga Putra Pratama
+Dinda Thalia Fahira
+Indraswara Galih Jayanegara
+Micky Valentino
+Renaldy Arief Susanto
+Devinzen
+Winata Tristan
+Salsabiila
+Anthony Bryant Gouw
+Dhafin Fawwaz Ikramullah
+Rayendra Althaf Taraka Noor
+Amalia Putri
+M. Hanief Fatkhan Nashrullah
+Agil Fadillah Sabri
+Harry Truman Suhalim
+Wisyendra Lunarmalam
+Dzulfaqor Ali Dipangegara
+Richie Leonardo
+Raffael Boymian Siahaan
+Willhelmina Rachel Silalahi
+Bryan Cornelius Lauwrence
+Zahira Dina Amalia
+Eduardus Alvito Kristiadi
+Ellijah Darrellshane S.
+William Glory Henderson
+Dahayu Ramaniya Aurasindu
+Ahmad Hasan Albana
+Yasra Zhafirah
+Kharris Khisunica
+Imam Hanif Mulyarahman
+Muhamad Rafli Rasyiidin
+Julian Caleb Simandjuntak
+Andhika Tantyo Anugrah
+Venantius Sean Ardi Nugroho
+Dzaky Satrio Nugroho
+Arvyno Pranata Limahardja
+Bihurin Salsabila Firdaus
+Elbert Chailes
+Fedrianz Dharma
+Guntur Oktavianto Nugroho
+Irfan Sidiq Permana
+Sa'ad Abdul Hakim
+Muhammad Naufal Aulia
+Matthew Lim
+Matthew Vladimir Hutabarat
+Novelya Putri Ramadhani
+Julian Chandra Sutadi
+Muhammad Rafly
+Bastian H Suryapratama
+Henrikus Nikolas Ardi
+Panji Sri Kuncara Wisma
+Givari Al Fachri
+Alfandito Rais Akbar
+Nafisha Virgin
+Melati Anggraini
+Satria Wisnu Wibowo
+Yovanka Sandrina Maharaja
+Dave Jansen Ongkaputra
+Juan Sohuturon Arauna Siagian
+Aurelius Justin Philo Fanjaya
+Dedy Hofmanindo Saragih
+Zachary Samuel Tobing
+Dewantoro Triatmojo
+Diero Arga Purnama
+Christopher Richard Chandra
+Diana Tri Handayani
+Lutfi Khairul Amal
+Akmal Galih Aji Sugmo Seno
+Muhammad Rafi Dhiyaulhaq
+Muhammad Ridho Rabbani
+Muhammad Dava Fathurrahman
+Daffa Ramadhan Elengi
+Eldwin Pradipta
+Muhammad Rifa Ansyari
+Monica Angela Hartono
+Farhan Nafis Rayhan
+Muhammad Neo Cicero Koda
+Azmi Mahmud Bazeid
+Kresna Adi Prayogo
+Haikal Assyauqi
+Muhammad Adli Arindra
+Syakira Fildza Nazhifan
+Ariel Herfrison
+Maximilian Suganeshaiyo
+Muhammad Raihan Ariffianto
+Dhidit Abdi Aziz
+Maria Flora Renata S
+Alessandro Jusack Hasian
+Muhammad Syarafi Akmal
+Timotius Vivaldi Gunawan
+Farah Aulia
+Shulha
+Eleanor Cordelia
+Andhita Naura Hariyanto
+Marvin Scifo Y. Hutahaean
+Raden Rafly Hanggaraksa B
+Muhammad Faiz Al Muhtadin
+Vini Putiasa
+Mattheuw Suciadi Wijaya
+Bagas Sambega Rosyada
+Dimas Bagoes Hendrianto
+Nyoman Ganadipa Narayana
+Berto Richardo Togatorop
+Mesach Harmasendro
+Theo Livius Natael
+Mario Hermawan Setiadi
+Irfan Musthofa
+Erdianti Wiga Putri Andini
+Olivia Christy Lismanto
+Zaki Yudhistira Candra
+Ervina Limka
+Filbert Fuvian
+Randy Verdian
+Alifia Sariningtyas Wulandari
+Daffari Adiyatma
+Kayla Namira Mariadi
+Risma Rif'atissania
+Steven Adrian Corne
+Benedicta Eryka Santosa
+Christoper Daniel
+Shafiq Irvansyah
+Matthew Nicholas Gunawan
+Derwin Rustanly
+Justin Lawrance
+Taufiq Ramadhan Ahmad
+Bastian Natanael Sibarani
+Benjamin Sihombing
+Lydia Gracia
+Ahmad Farid Mudrika
+Qady Zakka Raymaula
+Kezia Caren Cahyadi
+Steven Tjhia
+Muhammad Atpur Rafif
+Louis Ferdyo Gunawan
+Kayla Dyara
+Anfasa Kamil Wibowo
+Muhammad Nurul Hakim
+Marzuli Suhada M
+Abdul Rafi Radityo Hutomo
+Nasywaa Anggun Athiefah
+Muhammad Naufal Rayhan Wibawa
+Mohammad Yuzer Irosoneri
+Muhammad Arif
+Matthew Hotmaraja Johan Turnip
+Arya Kusuma Daniswara
+Eka Devi Shavitri"""
+
+nim_ganesha = """19622001
+19622002
+19622003
+19622004
+19622005
+19622006
+19622007
+19622008
+19622009
+19622010
+19622011
+19622012
+19622013
+19622014
+19622015
+19622016
+19622017
+19622018
+19622019
+19622020
+19622021
+19622022
+19622023
+19622024
+19622025
+19622026
+19622027
+19622028
+19622029
+19622030
+19622031
+19622032
+19622033
+19622034
+19622035
+19622036
+19622037
+19622038
+19622039
+19622040
+19622041
+19622042
+19622043
+19622044
+19622045
+19622046
+19622047
+19622048
+19622049
+19622050
+19622051
+19622052
+19622053
+19622054
+19622055
+19622056
+19622057
+19622058
+19622059
+19622060
+19622061
+19622062
+19622063
+19622064
+19622065
+19622066
+19622067
+19622068
+19622069
+19622070
+19622071
+19622072
+19622073
+19622074
+19622075
+19622076
+19622077
+19622078
+19622079
+19622080
+19622081
+19622082
+19622083
+19622084
+19622085
+19622086
+19622087
+19622088
+19622089
+19622090
+19622091
+19622092
+19622093
+19622094
+19622095
+19622096
+19622097
+19622098
+19622099
+19622100
+19622101
+19622102
+19622103
+19622104
+19622105
+19622106
+19622107
+19622108
+19622109
+19622110
+19622111
+19622112
+19622113
+19622114
+19622115
+19622116
+19622117
+19622118
+19622119
+19622120
+19622121
+19622122
+19622123
+19622124
+19622125
+19622126
+19622127
+19622128
+19622129
+19622130
+19622131
+19622132
+19622133
+19622134
+19622135
+19622136
+19622137
+19622138
+19622139
+19622140
+19622141
+19622142
+19622143
+19622144
+19622145
+19622146
+19622147
+19622148
+19622149
+19622150
+19622151
+19622152
+19622153
+19622154
+19622155
+19622156
+19622157
+19622158
+19622159
+19622160
+19622161
+19622162
+19622163
+19622164
+19622165
+19622166
+19622167
+19622168
+19622169
+19622170
+19622171
+19622172
+19622173
+19622174
+19622175
+19622176
+19622177
+19622178
+19622179
+19622180
+19622181
+19622182
+19622183
+19622184
+19622185
+19622186
+19622187
+19622188
+19622189
+19622190
+19622191
+19622192
+19622193
+19622194
+19622195
+19622196
+19622197
+19622198
+19622199
+19622200
+19622201
+19622202
+19622203
+19622204
+19622205
+19622206
+19622207
+19622208
+19622209
+19622210
+19622211
+19622212
+19622213
+19622214
+19622215
+19622216
+19622217
+19622218
+19622219
+19622220
+19622221
+19622222
+19622223
+19622224
+19622225
+19622226
+19622303
+19622304
+19622309
+19622313
+19622314
+19622315
+19622316
+19622317
+19622318"""
+
+jatinangor = """Moh Afnan Fawaz
+Nicolas Jeremy Marulitua S
+Adinda Khairunnisa Indraputri
+Muhammad Rasheed Qais Tandjung
+Muhammad Faishal Putra
+Sekar Anindita Nurjadini
+Valentino Chryslie Triadi
+Ahmad Fawwazi
+Farrel Natha Saskoro
+Muhammad Fauzan Azhim
+Melissa Trenggono
+M. Kasyfil Aziz
+Jason Jahja
+Gracya Tio Damena Sidabutar
+Bryan P. Hutagalung
+Rajendra Farras Rayhan
+Fawwaz Aydin Mustofa
+Jeremy Deandito
+Muhammad Faishal Firdaus
+Ardra Rafif Sahasika
+Axel Santadi Warih
+Yusril Fazri Mahendra
+Justin Aditya Putra Prabakti
+Jonathan Emmanuel Saragih
+Fadian Alif Mahardika
+Ahmad Rafi Maliki
+Muhammad Davis Adhipramana
+Jason Fernando
+Rafif Ardhinto Ichwantoro
+Hugo Sabam Augusto
+Muhammad Zaki
+Rizqika Mulia Pratama
+Muhammad Reffy Haykal
+Yosef Rafael Joshua
+Anindita Widya Santoso
+Audra Zelvania Putri Harjanto
+Muhammad Zaidan Sa'dun Robbani
+Alfaza Naufal Zakiy
+Attara Majesta Ayub
+Christian Justin Hendrawan
+Mohamad Maulana Firdaus R
+Muhammad Fatihul Irhab
+Maulvi Ziadinda Maulana
+Owen Tobias Sinurat
+Regan Adiesta Mahendra
+Angelica Aliwinata
+Khayla Belva Annandira
+Satriadhikara Panji Yudhistira
+Farhan Raditya Aji
+Chelvadinda
+Hanan Fitra Salam
+Ikhwan Al Hakim
+Chairul Nur Wahid
+Hafizh Hananta Akbari
+Muhammad Dzaki Arta
+Rayhan Ridhar Rahman
+Atqiya Haydar Luqman
+Nathaniel Liady
+Pradipta Rafa Mahesa
+Lina Azizah R.H.
+Andi Marihot Sitorus
+Muhammad Daffa Kusuma Atmaja
+Mohammad Andhika Fadillah
+Samuel Franciscus Togar H
+Gabriel Marcellino Joestiawan
+Gymnastiar Anwar
+Albert Ghazaly
+Auralea Alvinia Syaikha
+Rizqi Andhika Pratama
+Aland Mulia Pratama
+Mohammad Akmal Ramadan
+Nicholas Reymond Sihite
+Aqila Ataa
+Yasmin Farisah Salma
+Timothy Haposan Simanjuntak
+M Rivaldi Mahari
+Ahmad Thoriq Saputra
+Muhammad Roihan
+Samy Muhammad Haikal
+Shabrina Maharani
+Maulana Muhamad Susetyo
+Jimly Nur Arif
+Salsabila Azzahra"""
+
+nim_jatinangor = """19622227
+19622228
+19622229
+19622230
+19622231
+19622232
+19622233
+19622234
+19622235
+19622236
+19622237
+19622238
+19622239
+19622240
+19622241
+19622242
+19622243
+19622244
+19622245
+19622246
+19622247
+19622248
+19622249
+19622250
+19622251
+19622252
+19622253
+19622254
+19622255
+19622256
+19622257
+19622258
+19622259
+19622260
+19622261
+19622262
+19622263
+19622264
+19622265
+19622266
+19622267
+19622268
+19622269
+19622270
+19622271
+19622272
+19622273
+19622274
+19622275
+19622276
+19622277
+19622278
+19622279
+19622280
+19622281
+19622282
+19622283
+19622284
+19622285
+19622286
+19622287
+19622288
+19622289
+19622290
+19622291
+19622292
+19622293
+19622294
+19622295
+19622296
+19622297
+19622298
+19622299
+19622300
+19622301
+19622302
+19622305
+19622306
+19622307
+19622308
+19622310
+19622311
+19622312"""
+
+ganesha = ganesha.split("\n")
+nim_ganesha = nim_ganesha.split("\n")
+len_ganesha = len(ganesha)
+print("Jumlah ganesha:", len_ganesha, len(nim_ganesha))
+
+jatinangor = jatinangor.split("\n")
+nim_jatinangor = nim_jatinangor.split("\n")
+len_jatinangor = len(jatinangor)
+print("Jumlah jatinangor:", len_jatinangor, len(nim_jatinangor))
+
+kelompok = [[] for i in range(23)]
+
+def listOfTuples(l1, l2, string):
+    return list(map(lambda x, y:[x,y, string], l1, l2))
+
+list_ganesha = listOfTuples(nim_ganesha, ganesha, "Ganesha")
+list_jatinangor = listOfTuples(nim_jatinangor, jatinangor, "Jatinangor")
+
+# 19 Kelompok pertama 10 ganesha
+# 20-23 9 ganesha
+# Semua 4 jatinangor
+
+no_kel = 0
+for i in range(len_ganesha):
+    kelompok[no_kel].append(list_ganesha.pop(np.random.randint(len_ganesha)))
+    len_ganesha -= 1
+    no_kel = (no_kel + 1) % 23 
+
+no_kel = 22
+
+for i in range(len_jatinangor):
+    kelompok[no_kel].append(list_jatinangor.pop(np.random.randint(len_jatinangor)))
+    len_jatinangor -= 1
+    no_kel = (no_kel - 1) % 23 
+
+for i in range(len(kelompok)):
+    print(i+1, len(kelompok[i]))
+
+print(len(ganesha), len(jatinangor))
+
+# import xlsxwriter module
+import xlsxwriter
+
+# Workbook() takes one, non-optional, argument
+# which is the filename that we want to create.
+workbook = xlsxwriter.Workbook('plotting.xlsx')
+
+# The workbook object is then used to add new
+# worksheet via the add_worksheet() method.
+worksheet = workbook.add_worksheet()
+
+# Start from the first cell.
+# Rows and columns are zero indexed.
+row = 0
+column = 0
+ 
+for i in range(len(kelompok)):
+    # write operation perform
+    worksheet.write(row, column, i+1)
+    column += 1
+    for j in kelompok[i]:
+        worksheet.write(row, column, j[0])
+        worksheet.write(row, column+1, j[1])
+        worksheet.write(row, column+2, j[2])
+        row += 1
+    column -= 1
+    
+     
+workbook.close()
